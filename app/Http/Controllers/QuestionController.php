9ab6d1578d256ad3a4950fcf49fdfa3524e81243
Petr Stoklas <petrstoklas@gmail.com>
2019-02-22 09:47:53 +0100

    modelas added

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// use DB;
+use App\Question;
+use App\Answer;
+
 class QuestionController extends Controller
 {
     //
     public function index()
     {
+
+        $all_questions = Question::all();
+        dd($all_questions);
         return 'My QuestionController is this';
     }
 
     public function show($id)
     {
+
+        $question = Question::find($id);
+        $answers = $question->answers()->oldest()->get();
+
+        dd($answers);
         return 'This is question detail of question with id '.$id ;
     }
 }
